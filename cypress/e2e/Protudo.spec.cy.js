869b338ab9003645b14f2ee201bb824d4a0bf16c
Igor Gallette <igorcouto40@yahoo.com.br>
2022-08-25 21:15:33 -0300

    Testes de produtos adicionados

---
+++ cypress/e2e/Protudo.spec.cy.js
@@ -0,0 +1,28 @@
+/// <reference types='cypress' />
+
+describe('Adicionando protudos no carrinho', () => {
+    beforeEach(() => {
+        cy.visit('produtos')
+    })
+
+    it('Adicionar produto no carrinho com sucesso', () => {
+        cy.addProduto('3', 'M', 'Green', '3')
+
+    });
+
+    it('Adicionar produto no carrinho com sucesso', () => {
+        cy.addProduto('6', 'M', 'Gray', '3')
+
+    });
+
+    it.only('Adicionar produto no carrinho fora de estoque', () => {
+        cy.get('[class="image-hover unveil-image"]').eq(2).click()
+        cy.get('.button-variable-item-34').click()
+        cy.get('.button-variable-item-Brown').click()
+        cy.get('.input-text').clear().type(4)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.stock').should('contain', 'Fora de estoque')
+
+    });
+
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('addProduto', (produto, tamanho, cor, quantidade) => {
+
+    cy.get('[class="image-hover unveil-image"]').eq(produto).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+})
